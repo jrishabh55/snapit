70a89cf27a9071947431bcca0b687eb482327931
Rishabh Jain <rishabh@jnexsoft.com>
2020-06-11 19:52:05 +0530

    feat: syntax highlighter component

---
+++ src/components/SyntaxHighlighter.tsx
@@ -0,0 +1,16 @@
+import React, { useMemo } from 'react';
+import ReactSyntaxHighlighter, { SyntaxHighlighterProps } from 'react-syntax-highlighter';
+import * as themes from 'react-syntax-highlighter/dist/esm/styles/hljs';
+
+const SyntaxHighlighter: React.FC<SyntaxHighlighterProps> = ({ children, style, ...rest }) => {
+  const $themes: any = themes;
+  const theme = useMemo(() => $themes[style], [style, $themes]);
+  return (
+    <ReactSyntaxHighlighter language="javascript" style={theme} showLineNumbers {...rest}>
+      {children}
+    </ReactSyntaxHighlighter>
+  );
+};
+
+
+export default SyntaxHighlighter;
