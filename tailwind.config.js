2a02543712878f472ad022168bda4ab1cd2cf879
Rishabh Jain <rishabh@codeation.io>
2020-06-15 00:30:13 +0530

    feat: tailwindcss update

--- tailwind.config.js
+++ tailwind.config.js
@@ -203,6 +203,7 @@ module.exports = {
       inner: 'inset 0 2px 4px 0 rgba(0, 0, 0, 0.06)',
       outline: '0 0 0 3px rgba(66, 153, 225, 0.5)',
       none: 'none',
+      normal: '0 1px 3px 0 rgba(0, 0, 0, 0.1), 0 1px 2px 0 rgba(0, 0, 0, 0.06)',
     },
     container: {},
     cursor: {
