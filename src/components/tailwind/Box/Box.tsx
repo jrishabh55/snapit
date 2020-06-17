0cc919b80361f6622dc852a528f1428bd36963da
Rishabh Jain <rishabh@codeation.io>
2020-06-18 03:14:43 +0530

    feat: added rounded prop in box

--- src/components/tailwind/Box/Box.tsx
+++ src/components/tailwind/Box/Box.tsx
@@ -1,5 +1,5 @@
 import React, { ReactNode, CSSProperties, FC, memo, useMemo } from 'react';
-import { ShadowType, DisplayTypes } from '../TailwindInterfaces';
+import { ShadowType, DisplayTypes, BorderTypes } from '../TailwindInterfaces';
 
 interface BoxProps {
   className?: string;
@@ -7,21 +7,25 @@ interface BoxProps {
   shadowType?: ShadowType;
   children: ReactNode;
   style?: CSSProperties;
+  rounded?: BorderTypes;
 }
 
-const Box: FC<BoxProps> = memo(({ children, className, style = {}, display = 'flex', shadowType = 'none' }) => {
-  const classes = useMemo(() => {
-    const arr: string[] = [display, `shadow-${shadowType}`];
-    if (className) arr.push(className);
+const Box: FC<BoxProps> = memo(
+  ({ children, rounded = 'none', className, style = {}, display = 'flex', shadowType = 'none' }) => {
+    const classes = useMemo(() => {
+      const arr: string[] = [display, `shadow-${shadowType}`];
+      if (rounded) arr.push(`rounded-${rounded}`);
+      if (className) arr.push(className);
 
-    return arr.join(' ');
-  }, [className, display, shadowType]);
+      return arr.join(' ');
+    }, [className, display, rounded, shadowType]);
 
-  return (
-    <div style={style} className={classes}>
-      {children}
-    </div>
-  );
-});
+    return (
+      <div style={style} className={classes}>
+        {children}
+      </div>
+    );
+  }
+);
 
 export default Box;
