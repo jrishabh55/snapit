bc41e90df0133759af1238a9a00558b33f056e82
Rishabh Jain <rishabh@codeation.io>
2020-06-25 01:20:37 +0530

    feat: switched SnapIt to its own page

--- src/App.tsx
+++ src/App.tsx
@@ -1,77 +1,15 @@
-import React, { useCallback, useState, useMemo, MouseEvent, ChangeEvent } from 'react';
-import domToImage from 'dom-to-image';
-import { saveAs } from 'file-saver';
-
-import CodeWrapper from 'components/CodeWrapper';
-import CodeMirror from 'components/CodeMirror/CodeMirror';
-
-import { themes, baseCode } from 'utils';
-import { modes } from 'utils/modes';
+import React from 'react';
 import Layout from 'components/Layout/Layout';
-import Box from 'components/tailwind/Box/Box';
-import Button from 'components/tailwind/Button';
-import Select from 'components/tailwind/Select';
 
 import './assets/main.css';
 import './App.scss';
+import SnapIt from 'pages/SnapIt';
 
 function App() {
-  const [theme, setTheme] = useState('material');
-  const [mode, setMode] = useState('javascript');
-
-  const onThemeChange = useCallback((e: ChangeEvent<HTMLSelectElement>) => {
-    setTheme(e.currentTarget.value);
-  }, []);
-
-  const onModeChange = useCallback((e: ChangeEvent<HTMLSelectElement>) => {
-    setMode(e.currentTarget.value);
-  }, []);
-
-  const onSnapIt = useCallback((e: MouseEvent<HTMLButtonElement>) => {
-    e.preventDefault();
-    const el = document.querySelector('#codeation-code-wrapper');
-    if (el) {
-      domToImage.toBlob(el).then((blob) => {
-        saveAs(blob, 'capture.png');
-      });
-    }
-  }, []);
-
-  const themeOption = useMemo(() => themes.map(($theme) => ({ name: $theme.name, value: $theme.id })), []);
-
-  const modeOption = useMemo(
-    () =>
-      modes.map(($language) => ({
-        name: $language.name,
-        value: $language.mode,
-        id: $language.id,
-      })),
-    []
-  );
-
   return (
     <div className="app">
       <Layout>
-        <Box className="max-w-screen-md mx-auto">
-          <Select options={themeOption} value={theme} onChange={onThemeChange} />
-          <Select options={modeOption} value={mode} onChange={onModeChange} />
-          <Button bgColor="white" textColor="blue-500" shadowType="normal" border rounded onClick={onSnapIt}>
-            Snap IT
-          </Button>
-        </Box>
-        <Box className="mt-4 max-w-screen-md mx-auto">
-          <CodeWrapper>
-            <CodeMirror
-              value={baseCode}
-              options={{
-                theme,
-                mode,
-                lineNumbers: true,
-                autofocus: true,
-              }}
-            />
-          </CodeWrapper>
-        </Box>
+        <SnapIt />
       </Layout>
     </div>
   );
