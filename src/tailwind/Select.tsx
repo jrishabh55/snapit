8e47295271045c02e6bf8809498de27329026d99
Rishabh Jain <rishabh@codeation.io>
2020-07-28 01:38:04 +0530

    feat: changes to select to allow fill width

--- src/tailwind/Select.tsx
+++ src/tailwind/Select.tsx
@@ -12,11 +12,12 @@ export const Option: FC<OptionProps> = ({ name, value }) => <option value={value
 export interface SelectProps extends BoxProps<HTMLSelectElement> {
   options: OptionProps[];
   value: number | string;
+  containerProps?: BoxProps<HTMLDivElement>;
 }
 
-const Select: FC<SelectProps> = ({ options, value, onChange }) => {
+const Select: FC<SelectProps> = ({ options, value, containerProps = {}, onChange }) => {
   return (
-    <Box display="inline-block" position="relative" className="w-64">
+    <Box display="inline-block" position="relative" {...containerProps}>
       <Box
         componentType="select"
         onChange={onChange}
@@ -24,7 +25,7 @@ const Select: FC<SelectProps> = ({ options, value, onChange }) => {
         display="block"
         rounded
         shadow
-        className="appearance-none w-full bg-white border border-gray-400 hover:border-gray-500 px-4 py-2 pr-8 leading-tight focus:outline-none focus:shadow-outline"
+        className="appearance-none max-w-full bg-white border border-gray-400 hover:border-gray-500 px-4 py-2 pr-8 leading-tight focus:outline-none focus:shadow-outline"
       >
         {options.map((option) => (
           <Option key={option.id || option.value} name={option.name} value={option.value} />
